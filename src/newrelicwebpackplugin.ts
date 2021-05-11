ef04fcb7beaa52458bcf0ed4743131b8535c288c
Peter Lockett <cerwym@googlemail.com>
2021-05-11 16:17:06 +0100

    feat: trying oput new hooks

--- src/newrelicwebpackplugin.ts
+++ src/newrelicwebpackplugin.ts
@@ -2,14 +2,27 @@
   // import "core-js/fn/array.find"
   // ...
 
-import { Compilation, Compiler, Stats } from 'webpack';
+import { Compilation, Compiler, Stats, StatsAsset } from 'webpack';
+import * as fs from 'fs'
 
 export interface Options {
   verbose: boolean
 }
 
+const isJavascript = function (asset: any) {
+  if(!asset['existsAt']) { return false }
+  if(!asset.existsAt.split) { return false }
+  const parts = asset.existsAt.split('.')
+  const extension = parts.pop()
+  return extension === 'js'
+}
+
+const pluginName = 'cerwym-newrelic-webpack-plugin';
+
 class NewrelicWebpackPlugin {
   private readonly verbose: boolean;
+
+  private assetsToPrepend: string[] = []
 
   constructor(options: Options = {verbose: true}) {
     this.log('Loaded');
@@ -23,7 +36,7 @@ class NewrelicWebpackPlugin {
   apply(compiler: Compiler) {
     const hooks = compiler.hooks;
 
-    hooks.done.tap('boundary-newrelic-webpack-plugin', stats => {
+    hooks.done.tap(pluginName, stats => {
       this.handleDone(stats);
     });
 
@@ -34,6 +47,34 @@ class NewrelicWebpackPlugin {
 
   handleDone(stats: Stats) {
     this.log('Running after compile hook', stats);
+
+    if (!stats.compilation.assets) {
+      console.log('No Compilation Assets exist')
+    }
+      // let's shorten that up
+      const assets = stats.compilation.assets
+
+      // get a list of all the possibly emitted asset files
+      const keys = Object.keys(stats.compilation.assets)
+      for (const key of keys) {
+        let asset = assets[key]
+        // if the asset hasn't been emitted or is not a
+        // javascript fie, then vail
+        if(!asset) { continue }
+        if(!isJavascript(asset)) { continue }
+
+        // put the agent at the top of the compiled file
+        const newSource = "require('newrelic');" + asset.source()
+        // @ts-ignore
+        fs.writeFile(asset.existsAt, newSource, function(err){
+          if(err) throw err;
+          // let the world know!
+          // TODO: is there a webpack output function this
+          //       should be going through
+          // @ts-ignore
+          console.log("\nAdded newrelic to " +  asset.existsAt + "\n")
+        })
+      }
   }
 
   log(message: string, ...optionalParams : any[]) {
