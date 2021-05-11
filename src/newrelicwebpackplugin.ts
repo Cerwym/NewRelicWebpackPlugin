f09c7e9d97ab7ae24d90cd5d7f6677142b7344a3
Peter Lockett <cerwym@googlemail.com>
2021-05-11 15:49:31 +0100

    fix: corrected dist

--- src/newrelicwebpackplugin.ts
+++ src/newrelicwebpackplugin.ts
@@ -1,6 +1,49 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
   // import "core-js/fn/array.find"
   // ...
-export default class DummyClass {
 
+import { Compilation, Compiler, Stats } from 'webpack';
+
+export interface Options {
+  verbose: boolean
 }
+
+class NewrelicWebpackPlugin {
+  private readonly verbose: boolean;
+
+  constructor(options: Options = {verbose: true}) {
+    this.log('Loaded');
+
+    this.verbose = options.verbose;
+
+    this.apply = this.apply.bind(this);
+    this.handleDone = this.handleDone.bind(this);
+  }
+
+  apply(compiler: Compiler) {
+    const hooks = compiler.hooks;
+
+    hooks.done.tap('boundary-newrelic-webpack-plugin', stats => {
+      this.handleDone(stats);
+    });
+
+    this.log('Applying hooks');
+
+    // compiler.hooks.done.tap(pluginName, appendNewrelicToJsAssets);
+  }
+
+  handleDone(stats: Stats) {
+    this.log('Running after compile hook', stats);
+  }
+
+  log(message: string, ...optionalParams : any[]) {
+    if (this.verbose) {
+      console.log(
+        `boundary-newrelic-webpack-plugin : ${message}`,
+        optionalParams
+      );
+    }
+  }
+}
+
+export {NewrelicWebpackPlugin}
